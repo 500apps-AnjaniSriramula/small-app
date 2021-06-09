efafc574f764b8b8c8739a3d5e0002e7fcb6e13d
500apps-AnjaniSriramula <84906862+500apps-AnjaniSriramula@users.noreply.github.com>
2021-06-09 09:39:42 +0530

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue-router'
+import Home from '../views/Home'
+import About from '../views/About'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+export default router
