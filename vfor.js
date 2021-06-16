4589ab0f4a40488a27791189b25e8cc6f89b282f
500apps-AnjaniSriramula <84906862+500apps-AnjaniSriramula@users.noreply.github.com>
2021-06-16 18:45:44 +0530

    Add files via upload

---
+++ vfor.js
@@ -0,0 +1,9 @@
+new Vue({
+    el:"#vfor",
+    data:{
+        items:['milk','butter','bread'],
+        persons:[
+            {name:'sam',age:'23',color:'red'},
+            {name:'anna',age:'unknown',color:'blue'}]
+    }
+});
---
+++ vif.js
@@ -0,0 +1,6 @@
+new Vue({
+    el:"#vif",
+    data:{
+        show:true
+    }
+});
