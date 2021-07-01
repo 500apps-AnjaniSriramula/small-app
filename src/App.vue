790bf770081f1288457e22fc72ef21a2a3071506
500apps-AnjaniSriramula <84906862+500apps-AnjaniSriramula@users.noreply.github.com>
2021-07-01 19:28:47 +0530

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-    <div>
-        <add-blog></add-blog>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Routing</h1>
+                <hr>
+                <router-view name="header-top"></router-view>
+                <transition name="slide" mode="out-in">
+                    <router-view></router-view>
+                </transition>
+                <router-view name="header-bottom"></router-view>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-// Imports
-import addBlog from './components/addBlog.vue';
-export default {
-    components: {
-        'add-blog': addBlog
-    },
-    data () {
-        return {
+    import Header from './components/header.vue';
+    export default {
+        components: {
+            appHeader: Header
         }
-    },
-    methods: {
     }
-}
 </script>
 
 <style>
-body{
-    margin: 0;
-    font-family: 'Nunito SemiBold';
-}
+    .slide-leave-active {
+        transition: opacity 1s ease;
+        opacity: 0;
+        animation: slide-out 1s ease-out forwards;
+    }
+
+    .slide-leave {
+        opacity: 1;
+        transform: translateX(0);
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
+
+    @keyframes slide-out {
+        0% {
+            transform: translateY(0);
+        }
+        100% {
+            transform: translateY(-30px);
+        }
+    }
+
+    @keyframes slide-in {
+        0% {
+            transform: translateY(-30px);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
 </style>
